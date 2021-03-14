6f7e3d27b06540cf61dda5924a44c60b034b2009
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-03-13 23:07:46 -0500

    banner added

---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Banner
+
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import requests from './requests';
 import './Row.css';
 const base_url= "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies]=useState([]);
 
     useEffect(() => {
@@ -26,13 +26,17 @@ console.table(movies);
     return (
         <div className="row">
             <h2>{title}</h2>
-            <div className="row_posters">
+            <div className="row__posters">
                 {movies.map(movie =>(
              <img 
-             className="row_poster"
-             src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+             key={movie.id}
+             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+             src={`${base_url}${
+                 isLargeRow ? movie.poster_path: movie.backdrop_path
+                }`} 
+             alt={movie.name}/>
 
-))}
+         ))}
 
             </div>
             {/* */}
@@ -40,5 +44,5 @@ console.table(movies);
     )
 }
 
-export default Row
+export default Row;
  
