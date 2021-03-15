1cf5cf5ae6bfbd8769c5dd22418576adae8fbf27
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-03-15 00:38:36 -0400

    firebase configuring

--- src/Row.js
+++ src/Row.js
@@ -2,11 +2,13 @@ import React,{useState, useEffect} from 'react'
 import YouTube from 'react-youtube';
 import axios from './axios';
 import requests from './requests';
+import movieTrailer from 'movie-trailer';
 import './Row.css';
 const base_url= "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies]=useState([]);
+    const [trailerUrl, setTrailerUrl]= useState("");
 
     useEffect(() => {
        async function fetchData(){
@@ -19,7 +21,25 @@ function Row({title, fetchUrl, isLargeRow}) {
       
     }, [fetchUrl]);
 
-console.table(movies);
+const opts={
+    height:"390",
+    width:"100%",
+    playerVars:{
+        autoplay:1, 
+    }, 
+};
+
+const handleClick = (movie) =>{
+   if(trailerUrl){
+       setTrailerUrl('');
+   }else{
+       movieTrailer(movie?.name || "")
+       .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+       }).catch((error) => console.log(error));
+   }
+}
 
 
 
@@ -31,6 +51,7 @@ console.table(movies);
                 {movies.map(movie =>(
              <img 
              key={movie.id}
+             onClick={() => handleClick(movie)}
              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
              src={`${base_url}${
                  isLargeRow ? movie.poster_path: movie.backdrop_path
@@ -40,7 +61,7 @@ console.table(movies);
          ))}
 
             </div>
-            <YouTube videoId={trailerUrl} opts={opts}/>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
             {/* */}
         </div>
     )
